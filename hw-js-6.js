778103ce902337b61ff67afbaf9692eff5104ea4
Алексей Савельев <mtptz21@gmail.com>
2023-05-22 22:25:51 +0300

    refactor: product page

---
+++ hw-js-6.js
@@ -0,0 +1,52 @@
+// 1.
+// let str = 'aaa@bbb@ccc';
+// let newstr = str.replace(/@/gi, '!');
+// console.log(newstr);
+
+// 2.
+// let date = '2025-12-31';
+// let slashDate = date.replace(/-/g, '/');
+// console.log(slashDate);
+
+// 3.
+// let str = 'Я учу javascript!';
+// let substrMethod = str.substr(2, 3);
+// let substrMethod1 = str.substr(6, 10);
+// console.log(substrMethod);
+// console.log(substrMethod1);
+
+// let substringMethod = str.substring(2, 5);
+// let substringMethod1 = str.substring(6, 16);
+// console.log(substringMethod);
+// console.log(substringMethod1);
+
+// let sliceMethod = str.slice(2, 5);
+// let sliceMethod1 = str.slice(6, 16);
+// console.log(sliceMethod);
+// console.log(sliceMethod1);
+
+// 4.
+// let arr = [4, 2, 5, 19, 13, 0, 10];
+// let sum = 0;
+// for (let i in arr) {
+//   arr[i] = arr[i] ** 3;
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(Math.sqrt(sum));
+
+// 5.
+// let a = 3;
+// let b = 5;
+// let c = a - b;
+// if (c < 1) {
+//   c = 1;
+// }
+// console.log(c);
+
+// 6.
+// Выведите на экран текущую дату-время в формате 12:59:59 31.12.2014.
+// Для решения этой задачи напишите функцию, которая будет добавлять 0
+// перед днями и месяцами, которые состоят из одной цифры (из 1.9.2014
+// сделает 01.09.2014).
